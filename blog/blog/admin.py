aeb9771e7e25ac7369e32abb16aab739aa092694
Tortuvshin <toroo.byamba@gmail.com>
2016-01-06 05:29:19 +0800

    update

--- blog/blog/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from blog.models import Blog
-
-admin.site.register(Blog)
--- blog/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-urlpatterns = [
-    url(r'^all/$', 'blog.views.blogs'),
-    url(r'^get/(?P<blog_id>\d+)/$', 'blog.views.blog'),
-]
-
-
